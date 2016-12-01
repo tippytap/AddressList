f10a6b5c352746272b434fcdbd48b2ddf9150167
Erik Miller <emiller46@radford.edu>
2016-12-01 17:23:13 -0500

    Test cases

--- src/AddressListTest.java
+++ src/AddressListTest.java
@@ -16,6 +16,17 @@ import org.junit.Test;
 public class AddressListTest {
 
 	public static AddressList addresses;
+	public static final String[][] records = new String[][]{
+		{
+			"John Doe", "555-555-5555", "jdoe@email.com", "123 Main St. Christiansburg, VA 24073", "1987-02-04"
+		},
+		{
+			"Jane Doe", "555-555-5444", "jane.doe@email.com", "222 Main St. Christiansburg, VA 24073", "1987-02-05"
+		},
+		{
+			"Matt Doe", "555-555-5333", "mattd@email.com", "345 Main St. Christiansburg, VA 24073", "1987-02-06"
+		}
+	};
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -39,6 +50,8 @@ public class AddressListTest {
 	public void testIsEmpty() {
 		assertTrue(addresses.isEmpty());
 		// TODO add test for when one of the add to methods are working
+		//addresses.addToFront(name, tel, email, address, dob);
+		//assertFalse(addresses.isEmpty());
 	}
 
 	/**
@@ -46,7 +59,14 @@ public class AddressListTest {
 	 */
 	@Test
 	public void testAddToFront() {
-		fail("Not yet implemented");
+		addresses.addToFront(
+				"John Doe", 
+				"555-555-5555", 
+				"jdoe@email.com", 
+				"123 Main St. Christiansburg, VA 24073", 
+				"1987-02-04"
+		);
+		assertEquals("John Doe", addresses.nameByPhoneNumber("5555-555-5555"));
 	}
 
 	/**
@@ -54,7 +74,14 @@ public class AddressListTest {
 	 */
 	@Test
 	public void testAddToBack() {
-		fail("Not yet implemented");
+		addresses.addToFront(
+				"John Doe", 
+				"555-555-5555", 
+				"jdoe@email.com", 
+				"123 Main St. Christiansburg, VA 24073", 
+				"1987-02-04"
+		);
+		assertEquals("John Doe", addresses.nameByPhoneNumber("5555-555-5555"));
 	}
 
 	/**
@@ -62,7 +89,32 @@ public class AddressListTest {
 	 */
 	@Test
 	public void testReverseToString() {
-		fail("Not yet implemented");
+		String expected = 
+				"---------------------------\n"
+			  + "Matt Doe\n"
+			  + "555-555-5333\n"
+			  + "mattd@email.com\n"
+			  + "345 Main St. Christiansburg, VA 24073\n"
+			  + "1987-02-06\n"
+			  + "---------------------------\n"
+			  + "---------------------------\n"
+			  + "Jane Doe\n"
+			  + "555-555-5444\n"
+			  + "jane.doe@email.com\n"
+			  + "222 Main St. Christiansburg, VA 24073\n"
+			  + "1987-02-05\n"
+			  + "---------------------------\n"
+			  + "---------------------------\n"
+			  + "John Doe\n"
+			  + "555-555-5555\n"
+			  + "jdoe@email.com\n"
+			  + "123 Main St. Christiansburg, VA 24073\n"
+			  + "1987-02-04\n"
+			  + "---------------------------\n";
+		for(String[] record : records){
+			addresses.addToBack(record[0], record[1], record[2], record[3], record[4]);
+		}
+		assertEquals(expected, addresses.reverseToString());
 	}
 
 	/**
@@ -70,7 +122,11 @@ public class AddressListTest {
 	 */
 	@Test
 	public void testReverse() {
-		fail("Not yet implemented");
+		for(String[] record : records){
+			addresses.addToBack(record[0], record[1], record[2], record[3], record[4]);
+		}
+		addresses.reverse();
+		assertEquals(addresses.toString(), addresses.reverseToString());
 	}
 
 	/**
@@ -78,7 +134,10 @@ public class AddressListTest {
 	 */
 	@Test
 	public void testSizeOf() {
-		fail("Not yet implemented");
+		for(String[] record : records){
+			addresses.addToBack(record[0], record[1], record[2], record[3], record[4]);
+		}
+		assertTrue(addresses.sizeOf() == 3);
 	}
 
 	/**
@@ -86,7 +145,11 @@ public class AddressListTest {
 	 */
 	@Test
 	public void testPhoneNumberByName() {
-		fail("Not yet implemented");
+		for(String[] record : records){
+			addresses.addToBack(record[0], record[1], record[2], record[3], record[4]);
+		}
+		String expected = "555-555-5555";
+		assertEquals(expected, addresses.phoneNumberByName("John Doe"));
 	}
 
 	/**
@@ -94,7 +157,11 @@ public class AddressListTest {
 	 */
 	@Test
 	public void testEmailByName() {
-		fail("Not yet implemented");
+		for(String[] record : records){
+			addresses.addToBack(record[0], record[1], record[2], record[3], record[4]);
+		}
+		String expected = "jane.doe@email.com";
+		assertEquals(expected, addresses.emailByName("Jane Doe"));
 	}
 
 	/**
@@ -102,7 +169,11 @@ public class AddressListTest {
 	 */
 	@Test
 	public void testNameByPhoneNumber() {
-		fail("Not yet implemented");
+		for(String[] record : records){
+			addresses.addToBack(record[0], record[1], record[2], record[3], record[4]);
+		}
+		String expected = "Matt Doe";
+		assertEquals(expected, addresses.nameByPhoneNumber("555-555-5333"));
 	}
 
 	/**
@@ -110,7 +181,11 @@ public class AddressListTest {
 	 */
 	@Test
 	public void testDobByName() {
-		fail("Not yet implemented");
+		for(String[] record : records){
+			addresses.addToBack(record[0], record[1], record[2], record[3], record[4]);
+		}
+		String expected = "1987-02-04";
+		assertEquals(expected, addresses.dobByName("John Doe"));
 	}
 
 }
